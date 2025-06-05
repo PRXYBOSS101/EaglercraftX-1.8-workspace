9dda423cc2be2f19bc9b96d902aa463622287725
PRXYBOSS101 <96063865+PRXYBOSS101@users.noreply.github.com>
2025-06-05 19:59:07 -0400

    Create BlastFurnaceBlock.java

---
+++ src/game/java/net/minecraft/block/BlastFurnaceBlock.java
@@ -0,0 +1,99 @@
+package net.minecraft.block;
+
+import net.lax1dude.eaglercraft.v1_8.EaglercraftRandom;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.world.World;
+import net.minecraft.world.level.block.AbstractFurnaceBlock;
+
+/**+
+ * This portion of EaglercraftX contains deobfuscated Minecraft 1.8 source code.
+ * 
+ * Minecraft 1.8.8 bytecode is (c) 2015 Mojang AB. "Do not distribute!"
+ * Mod Coder Pack v9.18 deobfuscation configs are (c) Copyright by the MCP Team
+ * 
+ * EaglercraftX 1.8 patch files (c) 2022-2025 lax1dude, ayunami2000. All Rights Reserved.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+public class BlastFurnaceBlock
+extends AbstractFurnaceBlock {
+    public static final MapCodec<BlastFurnaceBlock> CODEC = BlastFurnaceBlock.simpleCodec(BlastFurnaceBlock::new);
+
+    public MapCodec<BlastFurnaceBlock> codec() {
+        return CODEC;
+    }
+
+    protected BlastFurnaceBlock(BlockBehaviour.Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return new BlastFurnaceBlockEntity(blockPos, blockState);
+    }
+
+    @Override
+    @Nullable
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
+        return BlastFurnaceBlock.createFurnaceTicker(level, blockEntityType, BlockEntityType.BLAST_FURNACE);
+    }
+
+    @Override
+    protected void openContainer(Level level, BlockPos blockPos, Player player) {
+        BlockEntity blockEntity = level.getBlockEntity(blockPos);
+        if (blockEntity instanceof BlastFurnaceBlockEntity) {
+            player.openMenu((MenuProvider)((Object)blockEntity));
+            player.awardStat(Stats.INTERACT_WITH_BLAST_FURNACE);
+        }
+    }
+
+    @Override
+    public void animateTick(BlockState blockState, Level level, BlockPos blockPos, RandomSource randomSource) {
+        if (!blockState.getValue(LIT).booleanValue()) {
+            return;
+        }
+        double d = (double)blockPos.getX() + 0.5;
+        double d2 = blockPos.getY();
+        double d3 = (double)blockPos.getZ() + 0.5;
+        if (randomSource.nextDouble() < 0.1) {
+            level.playLocalSound(d, d2, d3, SoundEvents.BLASTFURNACE_FIRE_CRACKLE, SoundSource.BLOCKS, 1.0f, 1.0f, false);
+        }
+        Direction direction = blockState.getValue(FACING);
+        Direction.Axis axis = direction.getAxis();
+        double d4 = 0.52;
+        double d5 = randomSource.nextDouble() * 0.6 - 0.3;
+        double d6 = axis == Direction.Axis.X ? (double)direction.getStepX() * 0.52 : d5;
+        double d7 = randomSource.nextDouble() * 9.0 / 16.0;
+        double d8 = axis == Direction.Axis.Z ? (double)direction.getStepZ() * 0.52 : d5;
+        level.addParticle(ParticleTypes.SMOKE, d + d6, d2 + d7, d3 + d8, 0.0, 0.0, 0.0);
+    }
+}
+
